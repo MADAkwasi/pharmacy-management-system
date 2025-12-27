b928f4ceb7883cc60991d238fe90b5c2aa23e6b7
MADAkwasi <agyemanmda@gmail.com>
2025-12-27 04:38:46 +0000

    feat(dashboard): enhance layout with user info and icons, add lucide-angular dependency

--- src/app/core/layout/dashboard/dashboard.ts
+++ src/app/core/layout/dashboard/dashboard.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { LucideAngularModule, Pill } from 'lucide-angular';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [RouterOutlet, LucideAngularModule],
   templateUrl: './dashboard.html',
 })
 export class Dashboard {
-
+  protected readonly icons = {Pill}
 }
